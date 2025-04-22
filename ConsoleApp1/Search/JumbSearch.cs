23ebfe659c50ce425481dca2e0222b94f6424531
Raphael-Fug <nguyendoandong5@gmail.com>
2025-04-23 00:26:13 +0700

    JumbSearch\

---
+++ ConsoleApp1/Search/JumbSearch.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Search
+{
+    internal class JumbSearch
+    {
+        static int Search(int[] A, int k)
+        {
+            int n = A.Length;
+            int step = (int)Math.Sqrt(Math.Floor(Math.Sqrt(n)));
+            int i = 0;
+            int jumb = i + step;
+            while (i <= n && A[jumb] < k)
+            {
+                jumb += step;
+                i += step;
+            }
+            if (jumb > n) jumb = n - 1;
+            while (A[i] < k)
+            {
+                i ++;
+                if (i >= n || i > jumb) return -1; 
+            }
+            if (A[i] == k) return i;
+            else return -1;
+        }
+    }
+}
