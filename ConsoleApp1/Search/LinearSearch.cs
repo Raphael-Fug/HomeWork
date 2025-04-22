87e8c8b211b3734e3252015daaa3d3216b639ca8
Raphael-Fug <nguyendoandong5@gmail.com>
2025-04-22 16:31:11 +0700

    Search part 1

---
+++ ConsoleApp1/Search/LinearSearch.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Search
+{
+    internal class LinearSearch
+    {
+        static int Linear(int[] A, int k)
+        {
+            int n = A.Length;
+            for (int i = 0; i < n; i++)
+            {
+                if (A[i] == k) return i;
+            }
+            return -1;
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Nhập số lượng mảng: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] A = new int[n];
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write($"Nhập giá trị A{i+1}: ");
+                A[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.Write("Nhập giá trị K cần tìm: ");
+            int k = Convert.ToInt32(Console.ReadLine());
+            int Value = Linear(A, k);
+            Console.WriteLine($"Vị trí: {Value}");
+
+        }
+    }
+}
---
+++ ConsoleApp1/Search/SentinelSearch.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Search
+{
+    internal class SentinelSearch
+    {
+        static int Sentinel(int[] A, int k)
+        {
+            int n = A.Length;
+            A[n + 1] = k;
+            int i = 1;
+            while (A[i] != n) i ++;
+            return (i < n) ? i : -1;
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Nhập số lượng mảng: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] A = new int[n];
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write($"Nhập giá trị A{i + 1}: ");
+                A[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.Write("Nhập giá trị K cần tìm: ");
+            int k = Convert.ToInt32(Console.ReadLine());
+            int Value = Sentinel(A, k);
+            Console.WriteLine($"Vị trí: {Value}");
+        }
+    }
+}
