ee96146e7877914c47c5ba0a141f2865a6f0ae5c
Raphael-Fug <nguyendoandong5@gmail.com>
2025-04-24 09:20:11 +0700

    Class

---
+++ ConsoleApp1/InClass/Bac.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InClass
+{
+    internal class Bac
+    {
+        static string Bac1(int a, int b)
+        {
+            if (a == 0) return (b == 0) ? "Vô số nghiệm" : "Vô nghiệm";
+            else return $"Có 1 nghiệm là: {-b/a}";
+        }
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            Bac1(a, b);
+        }
+    }
+}
