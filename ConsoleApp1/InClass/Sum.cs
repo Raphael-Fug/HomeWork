0bcaf8663362a7801583fc7b4cc37e71cc184b5c
Raphael-Fug <nguyendoandong5@gmail.com>
2025-04-30 22:14:04 +0700

    BinarySearchC#

---
+++ ConsoleApp1/InClass/Sum.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InClass
+{
+    internal class Sum
+    {
+        static int Tong(int n)
+        {
+            return (n * (1 + n) / 2);
+        }
+        //static int Tong(int n)
+        //{
+        //    for (int i = 1; i <= n; i ++)
+
+        //}
+        static void Main()
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(Tong(n));
+        }
+    }
+}
