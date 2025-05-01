d004f21c5634bedc9bff73e611e6c4eeb91710ca
Raphael-Fug <nguyendoandong5@gmail.com>
2025-05-01 23:57:19 +0700

    SelectSort and BubleSort

---
+++ ConsoleApp1/Sort/BubleSort.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sort
+{
+    internal class BubleSort
+    {
+        static void Buble(int[] A)
+        {
+            for (int i = 0;  i < A.Length - 1; i++)
+            {
+                for (int j = 0; j < A.Length - 1; j++)
+                {
+                    if (A[j] > A[j + 1])
+                    {
+                        int temp = A[j];
+                        A[j] = A[j + 1];
+                        A[j + 1] = temp;
+                    }
+                }    
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Nhập số lượng mảng: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] A = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write($"Nhập giá trị A{i + 1}: ");
+                A[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Buble(A);
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(A[i] + " ");
+            }
+        }
+    }
+}
---
+++ ConsoleApp1/Sort/SelectionSort.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sort
+{
+    internal class SelectionSort
+    {
+        static void Selection(int[] A)
+        {
+            int n = A.Length;
+            for (int i = 0;  i < n-1; i++)
+            {
+                int i_min = i;
+                int v_min = A[i];
+                for (int j = i; j < n; j++)
+                {
+                    if (A[j] < v_min)
+                    {
+                        v_min = A[j];
+                        i_min = j;
+                    }
+                }
+                int temp = A[i];
+                A[i] = A[i_min];
+                A[i_min] = temp;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Nhập số lượng mảng: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] A = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write($"Nhập giá trị A{i + 1}: ");
+                A[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Selection(A);
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(A[i] + " ");
+            }
+        }
+    }
+}
